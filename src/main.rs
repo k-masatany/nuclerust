caa1f842a6796e4885ffadf1ec1d10ed43f37232
k-masatany <masatani@fusic.co.jp>
2017-10-12 17:52:44 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![feature(alloc)]
+#![feature(allocator_api)]
+#![feature(asm)]
+#![feature(collections)]
+#![feature(concat_idents)]
+#![feature(conservative_impl_trait)]
+#![feature(const_atomic_usize_new)]
+#![feature(const_fn)]
+#![feature(const_max_value)]
+#![feature(core_intrinsics)]
+#![feature(drop_types_in_const)]
+#![feature(global_allocator)]
+#![feature(integer_atomics)]
+#![feature(lang_items)]
+#![feature(naked_functions)]
+#![feature(never_type)]
+#![feature(thread_local)]
+#![feature(unique)]
+#![feature(const_size_of)]
+#![no_std]
+
+fn main() {
+    println!("Hello, world!");
+}
