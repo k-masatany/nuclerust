56a57ca185fc99743dd8be95a5afd204ffa16d52
k-masatany <masatani@fusic.co.jp>
2017-10-22 16:41:17 +0900

    ひとまずisoができたところ

--- target/rls/debug/libnuclerust.d
+++ target/rls/debug/libnuclerust.d
@@ -1,1 +1,1 @@
-/Users/kmasatany/workspace/nuclerust/target/rls/debug/libnuclerust.rmeta: /Users/kmasatany/workspace/nuclerust/src/lib.rs
+/Users/k-masatany/rust/nuclerust/target/rls/debug/libnuclerust.rmeta: /Users/k-masatany/rust/nuclerust/src/vga_buffer.rs /Users/k-masatany/rust/nuclerust/src/lib.rs
